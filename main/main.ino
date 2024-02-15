d18d729e5a91f0e8832b8e00fe4c360f8b981c15
Dirk Arnez <smalldirkalex@gmail.com>
2024-02-16 07:17:52 +0800

    - add main/main.ino

---
+++ main/main.ino
@@ -0,0 +1,32 @@
+#include <SoftwareSerial.h>
+
+const byte xPin = A0;
+const byte yPin = A1;
+
+SoftwareSerial ss(4, 7);
+
+void setup()
+{
+   Serial.begin(115200);
+   ss.begin(38400);
+}
+
+void loop()
+{
+   static unsigned long timer = 0;
+   unsigned long interval = 1000;
+   if (millis() - timer >= interval)
+   {
+      timer = millis();
+      int xValue = analogRead(xPin);
+      int yValue = analogRead(yPin);
+      Serial.print("x value = ");
+      Serial.print(xValue);
+      Serial.print("   y value = ");
+      Serial.println(yValue);
+      char buffer[15];
+      snprintf(buffer, 14, "%d,%d", xValue, yValue);
+      Serial.println(buffer);
+      ss.println(buffer);
+   }
+}
